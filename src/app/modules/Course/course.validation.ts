374d62875ef25dd7f73087bd14efd49903b75208
Rakibul Hasan <rakibulhasan0641@gmail.com>
2024-06-26 22:42:40 +0600

    createCourseValidationSchema added

---
+++ src/app/modules/Course/course.validation.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+const preRequisiteCoursesValidationSchema=z.object({
+    course:z.string(),
+    isDeleted:z.boolean().optional()
+
+})
+
+const createCourseValidationSchema = z.object({
+    body:z.object({
+        title:z.string(),
+        prefix:z.string(),
+        code:z.number(),
+        credit:z.number(),
+        preRequisiteCourses:z.array(preRequisiteCoursesValidationSchema)
+    })
+})
+
+export const CourseValidation={
+    createCourseValidationSchema
+}
