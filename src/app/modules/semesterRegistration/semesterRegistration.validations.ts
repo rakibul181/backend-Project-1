da675324b56d60b022fc6c3ea2be3f8897b01e7a
Rakibul Hasan <rakibulhasan0641@gmail.com>
2024-06-28 07:58:11 +0600

    semesterRegistrationValidationSchema added

---
+++ src/app/modules/semesterRegistration/semesterRegistration.validations.ts
@@ -0,0 +1,18 @@
+import { z } from "zod"
+import { semesterRegistrationStatus } from "./semesterRegistration.constant"
+ 
+const semesterRegistrationValidationSchema = z.object({
+     body:z.object({
+        academicSemester:z.string(),
+        status: z.enum([...semesterRegistrationStatus as [string, ...string[]]] ),
+          startDate:z.string().datetime(),
+          endDate: z.string().datetime(),
+          minCredit:z.number(),
+          maxCredit: z.number(),
+     })
+})
+
+export const semesterRegistrationValidation =  {
+    semesterRegistrationValidationSchema
+}
+    
